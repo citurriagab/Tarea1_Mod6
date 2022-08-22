1956360e47e10d10e536cba7f51905885eeb24c3
citurriagab <carlamoss84@gmail.com>
2022-08-21 22:57:13 -0400

    Update dataset.py

--- Desarrollo_M6_1_CIturriaga/codigos/dataset.py
+++ Desarrollo_M6_1_CIturriaga/codigos/dataset.py
@@ -1,43 +1,15 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "\n",
-    "DATA_SOURCE = \"https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data\"\n",
-    "LOCAL_PATH = \"./data/iris.data\"\n",
-    "COLUMN_NAMES = [\"sepal_length\", \"sepal_width\", \"petal_length\", \"petal_width\", \"species\"]\n",
-    "\n",
-    "\n",
-    "def load_iris_web(URL=DATA_SOURCE):\n",
-    "    \"\"\"Descarga desde el repositorio UCI el dataset iris y retorna un diccionario con el 'dataset', una tupla que\n",
-    "    contiene X (input) e y (output), además de las 'etiquetas'.\"\"\"\n",
-    "    print(\"Datos cargados con éxito\")\n",
-    "    df = pd.read_csv(URL, names=COLUMN_NAMES, header=None)\n",
-    "    X, y = df.iloc[:, 0:4], df.iloc[:, [4]]\n",
-    "    labels = {key: id for key, id in zip(y.species.unique(), [x for x in range(3)])}\n",
-    "    y = y.replace({\"species\": labels}).squeeze()\n",
-    "    return {\"dataset\": (X, y),\n",
-    "            \"labels\": labels}\n",
-    "\n",
-    "\n",
-    "def load_iris_local(PATH=LOCAL_PATH):\n",
-    "    \"\"\"Cargar el dataset desde una ruta en disco local. Específicamente un archivo guardado en la carpeta ./data\"\"\"\n",
-    "    print(\"Datos cargados con éxito\")\n",
-    "    return pd.read_csv(PATH, names=COLUMN_NAMES, header=None)"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pandas as pd
+
+DATA_SOURCE = "RESULTADO_POR_LOTE.csv"
+COLUMN_NAMES = ["FECHA","LOTE","UNIDAD","PLACA","TIPO","MARCA","MODELO","ANHO","TRACC","COMB","GTIA. MEC","MAG","KILOMETRAJE","ADJUDIC."]
+
+
+
+def load_result(data=DATA_SOURCE):
+    """Descarga desde el repositorio UCI el dataset iris y retorna un diccionario con el 'dataset', una tupla que
+    contiene X (input) e y (output), además de las 'etiquetas'."""
+    print("Datos cargados con éxito")
+    df = pd.read_csv(data, names=COLUMN_NAMES, header=None)
+    X, y = df.iloc[:, 0:14], df.iloc[:, [14]]
+    y = y.replace({"PRECIO_VENTA"}).squeeze()
+    return {"dataset": (X, y)}
